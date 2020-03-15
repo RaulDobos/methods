528388a18cae2e0ba87033f1f80aadd3cabb0bb2
RaulDobos <raulpriam@yahoo.com>
2020-03-15 10:38:57 +0200

    created basic mathematical operations

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,5 +1,7 @@
 package org.fasttrackit;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,35 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        double a, b, res;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insert a value for a: ");
+        a = scanner.nextDouble();
+
+        System.out.println("Insert a value for b: ");
+        b = scanner.nextDouble();
+
+        Operations operations = new Operations();
+
+        res = operations.add(a, b);
+        System.out.println("a + b result is: " + res);
+
+        res = operations.subtract(a, b);
+        System.out.println("a - b result is: " + res);
+
+        res = operations.subtract(b, a);
+        System.out.println("b - a result is: " + res);
+
+        res = operations.multiply(a, b);
+        System.out.println("a * b result is: " + res);
+
+        res = operations.divide(a, b);
+        System.out.println("a / b result is: " + res);
+
+        res = operations.divide(b, a);
+        System.out.println("b / a result is: " + res);
+
     }
 }
